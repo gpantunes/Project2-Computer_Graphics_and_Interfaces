d27455e91dbe6b6429b61d647e9e16dbfc7ba43a
Guilherme <gp.antunes@campus.fct.unl.pt>
2023-11-13 22:35:26 +0000

    corrigi a funçao da tecla r

--- proj2/app.js
+++ proj2/app.js
@@ -116,8 +116,8 @@ function setup(shaders)
                 ROTATION_ANGLE += 5;
                 break;
             case 'r':
-                angles.theta = 0;
-                angles.gamma = 0;
+                angles.theta = 50;
+                angles.gamma = 15;
                 break;
             case '+':
                 if(animation) speed *= 1.1;
